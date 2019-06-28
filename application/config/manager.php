4e0d85c37ad89fd82c037fdfc46143d6e9c03adb
holavarrieta <ho@ixaya.com>
2019-06-28 14:34:17 -0500

    Added manager config

---
+++ application/config/manager.php
@@ -0,0 +1,8 @@
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+
+//'dev' for devleopment or 'prod' for production
+$config['edition'] = 'dev';
+// $config['edition'] = 'prod';
+
+?>
