c6bb69955d9659807019ca7e771b95645dedb763
Gustavo Moya Ortiz <gm@ixaya.com>
2021-03-09 12:54:16 -0600

    Incorporated Amazon S3 Library to upload profile pictures, and business logic to cache those files to the server

--- application/config/development/manager.php
+++ application/config/development/manager.php
@@ -17,9 +17,15 @@ $config['project_name'] = 'Manager';
 //Ixaya/Manager HMVC PHP Framework
 $config['copyright'] = '&copy;  Ixaya Business SA de CV 2020, All Rights Reserved.';
 
+$config['aws_bucket'] = 'manager.vps102.ixaya.net';
+$config['aws_accesskey'] = 'AKIARWN4IYVJDNHHRJCY';
+$config['aws_secretkey'] = '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';
+
+/*
 $config['aws_bucket'] = '';
 $config['aws_accesskey'] = '';
 $config['aws_secretkey'] = '';
+*/
 
 
 ?>
