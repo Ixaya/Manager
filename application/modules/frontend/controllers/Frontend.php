1ff0846ba3cf217213747ba528b47d01629ffecd
Humberto Olavarrieta <ho@ixaya.com>
2021-09-17 17:53:29 -0500

    Cleanup

--- application/modules/frontend/controllers/Frontend.php
+++ application/modules/frontend/controllers/Frontend.php
@@ -1,89 +1,61 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Frontend extends Public_Controller {
+class Frontend extends Public_Controller
+{
 
 	public function index()
 	{
 		$this->webpage('frontend');
 	}
 
-	
+
 	public function webpage($slug)
 	{
-		
-		
-		
-		if($this->config->item('cache_enable'))
-		{
+		if ($this->config->item('cache_enable')) {
 			//Cache from config
 			$this->output->cache($this->config->item('cache_time'));
 		}
-		
+
 		$sections = [];
-		
-		
+
+
 		$data = null;
-		if($this->config->item('cache_enable'))
-		{
+		if ($this->config->item('cache_enable')) {
 			//cache enabled
 			$this->load->driver('cache', array('adapter' => 'apc', 'backup' => 'file'));
 			$data = $this->cache->get("frontend/webpage/$slug");
 		}
-		
-		
-		if (!$data)
-		{
-		        log_message('DEBUG', "Saving frontend/webpage/$slug to the cache");
 
-		        $this->load->model('admin/webpage');
-		        $webpage = $this->webpage->get_all('',"slug = '$slug' and kind = 1");
-		
-				if(!empty($webpage))
-				{
-					//cargar la sección referida en el webpage
-					$webpage_id = $webpage[0]['id'];
-					$this->load->model('admin/page_section');
-					$sections =  $this->page_section->get_all('',"webpage_id = '$webpage_id'", '', '', 'order asc');			
-				}
-				
-				//cargar los page_items de esa seccion
-				$this->load->model('admin/page_item');
-				foreach($sections as &$section)
-				{
-					$page_section_id = $section['id'];
-					$section['page_items'] = $this->page_item->get_all('',"page_section_id = $page_section_id");
-				}
 
-				$data['sections'] = $sections;
-						
-		        // Save into the cache for 5 minutes
-		        $this->cache->save("frontend/webpage/$slug", $data, 300);
+		if (!$data) {
+			log_message('DEBUG', "Saving frontend/webpage/$slug to the cache");
+
+			$this->load->model('admin/webpage');
+			$webpage = $this->webpage->get_all('', "slug = '$slug' and kind = 1");
+
+			if (!empty($webpage)) {
+				//cargar la sección referida en el webpage
+				$webpage_id = $webpage[0]['id'];
+				$this->load->model('admin/page_section');
+				$sections =  $this->page_section->get_all('', "webpage_id = '$webpage_id'", '', '', 'order asc');
+			}
+
+			//cargar los page_items de esa seccion
+			$this->load->model('admin/page_item');
+			foreach ($sections as &$section) {
+				$page_section_id = $section['id'];
+				$section['page_items'] = $this->page_item->get_all('', "page_section_id = $page_section_id");
+			}
+
+			$data['sections'] = $sections;
+
+			// Save into the cache for 5 minutes
+			$this->cache->save("frontend/webpage/$slug", $data, 300);
 		} else {
 			log_message('DEBUG', "Using frontend/webpage/$slug cache");
 		}
-		
-		$this->load_view('webpage',$data);
-	}
-	
-	public function test()
-	{
-		$this->load->library('admin/amazons3');
-		$aws_bucket = $this->config->item('aws_bucket');
-		$aws_accesskey = $this->config->item('aws_accesskey');
-		$aws_secretkey = $this->config->item('aws_secretkey');
-		
-		$this->amazons3->aws_bucket = $aws_bucket;
-		$this->amazons3->aws_accesskey = $aws_accesskey;
-		$this->amazons3->aws_secretkey = $aws_secretkey;
-		
-		
-		//$this->amazons3->upload_file('/home/manager/app/private/user/ef2d3c16686589d1dd8eff7b65a59cc8.jpg');
-		//$result = $this->amazons3->list_files();
-		$result = $this->amazons3->save_file('ef2d3c16686589d1dd8eff7b65a59cc8.jpg', '/home/manager/app/private/temp/');
-		print("<pre>");
-		print_r($result);
-		print("</pre>");
-	}
 
+		$this->load_view('webpage', $data);
+	}
 }
--- application/modules/private/controllers/Profile.php
+++ application/modules/private/controllers/Profile.php
@@ -1,7 +1,8 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Profile extends Private_Controller {
+class Profile extends Private_Controller
+{
 
 	public function index()
 	{
@@ -15,26 +16,24 @@ class Profile extends Private_Controller {
 
 		//cargar el webpage referidop
 		$this->load->model('admin/webpage');
-		$webpage = $this->webpage->get_all('',"slug = '$slug' and kind = 2");
+		$webpage = $this->webpage->get_all('', "slug = '$slug' and kind = 2");
 
-		if(!empty($webpage))
-		{
+		if (!empty($webpage)) {
 			//cargar la sección referida en el webpage
 			$webpage_id = $webpage[0]['id'];
 			$this->load->model('admin/page_section');
-			$sections =  $this->page_section->get_all('',"webpage_id = '$webpage_id'");
+			$sections =  $this->page_section->get_all('', "webpage_id = '$webpage_id'");
 		}
 		//cargar los page_items de esa seccion
 		$this->load->model('admin/page_item');
-		foreach($sections as &$section)
-		{
+		foreach ($sections as &$section) {
 			$page_section_id = $section['id'];
-			$section['page_items'] = $this->page_item->get_all('',"page_section_id = $page_section_id");
-// 			$section['page_items'] = [];
+			$section['page_items'] = $this->page_item->get_all('', "page_section_id = $page_section_id");
+			//$section['page_items'] = [];
 		}
-		$data ['sections'] = $sections;
+		$data['sections'] = $sections;
 		$_SESSION['page_title'] = $slug;
-		$this->load_view('webpage',$data);
+		$this->load_view('webpage', $data);
 	}
 	public function edit()
 	{
@@ -50,22 +49,19 @@ class Profile extends Private_Controller {
 			$data['first_name'] = $this->input->post('first_name');
 			$data['last_name'] = $this->input->post('last_name');
 			$data['email'] = $this->input->post('email');
-// 			$data['company'] = $this->input->post('company');
+			// 			$data['company'] = $this->input->post('company');
 
 			$newPassword = $this->input->post('password');
 			$password_confirmation = $this->input->post('password_confirmation');
-			if(!empty($newPassword) && $password_confirmation == $newPassword){
+			if (!empty($newPassword) && $password_confirmation == $newPassword) {
 				//if you use: ion_auth->update there is no need to encrypt it, else it will double crypt it.
 				$data['password'] = $newPassword;
 			}
 
-
-
 			//save profile picture image
 			$relative_path = "../private/user/";
 			$result = $this->upload_image($relative_path);
-			if(!empty($result['thumb_image_name']))
-			{
+			if (!empty($result['thumb_image_name'])) {
 				$data['image_name'] = $result['thumb_image_name'];
 				$data['image_url'] = base_url('private/profile/picture');
 
@@ -78,44 +74,55 @@ class Profile extends Private_Controller {
 				$this->amazons3->aws_accesskey = $aws_accesskey;
 				$this->amazons3->aws_secretkey = $aws_secretkey;
 
-				$this->amazons3->upload_file($relative_path.$result['thumb_image_name']);
-				$this->amazons3->upload_file($relative_path.$result['fullsize_image_name']);
+				$this->amazons3->upload_file($relative_path . $result['thumb_image_name']);
+				$this->amazons3->upload_file($relative_path . $result['fullsize_image_name']);
 			}
-
-
-
-
 
 			$this->ion_auth->update($current_user->id, $data);
 			$current_user = $this->ion_auth->user()->row();
 		}
 
 		$data['user'] = $current_user;
-		$this->load_view('profile_edit',$data);
+		$this->load_view('profile_edit', $data);
 	}
 	public function picture()
 	{
 		$current_user = $this->ion_auth->user()->row();
 		$relative_path = "../private/user/";
 		$filename = $current_user->image_name;
-		$file_path = FCPATH.$relative_path.$filename;
+		$file_path = FCPATH . $relative_path . $filename;
 
-		if(!file_exists($file_path))
-		{
+		if (!file_exists($file_path)) {
 			$this->load->library('admin/amazons3');
 
 			$this->amazons3->aws_bucket = $this->config->item('aws_bucket');
 			$this->amazons3->aws_accesskey = $this->config->item('aws_accesskey');
 			$this->amazons3->aws_secretkey = $this->config->item('aws_secretkey');
 			$result = $this->amazons3->save_file($filename, $relative_path);
-			log_message('debug',"Get File from S3 $filename");
+			log_message('debug', "Get File from S3 $filename");
 		} else {
-			log_message('debug',"Get File from Local FileSystem $filename");
+			log_message('debug', "Get File from Local FileSystem $filename");
 		}
 
-
-
-
 		$this->display_image($file_path);
+	}
+
+	public function test()
+	{
+		$this->load->library('admin/amazons3');
+		$aws_bucket = $this->config->item('aws_bucket');
+		$aws_accesskey = $this->config->item('aws_accesskey');
+		$aws_secretkey = $this->config->item('aws_secretkey');
+
+		$this->amazons3->aws_bucket = $aws_bucket;
+		$this->amazons3->aws_accesskey = $aws_accesskey;
+		$this->amazons3->aws_secretkey = $aws_secretkey;
+
+		//$this->amazons3->upload_file('/home/manager/app/private/user/ef2d3c16686589d1dd8eff7b65a59cc8.jpg');
+		//$result = $this->amazons3->list_files();
+		$result = $this->amazons3->save_file('ef2d3c16686589d1dd8eff7b65a59cc8.jpg', '/home/manager/app/private/temp/');
+		// print("<pre>");
+		// print_r($result);
+		// print("</pre>");
 	}
 }
