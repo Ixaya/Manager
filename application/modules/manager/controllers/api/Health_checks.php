d822597a62b0ed189d06944a7df79e4c35246990
Humole <221601+humole@users.noreply.github.com>
2026-01-21 10:52:58 -0600

    Cleanup

--- application/modules/manager/controllers/api/Health_checks.php
+++ application/modules/manager/controllers/api/Health_checks.php
@@ -28,46 +28,4 @@ class Health_checks extends REST_Controller
 	{
 		$this->response(['status' => -1, 'message' => "404 Not Found"], REST_Controller::HTTP_NOT_FOUND);
 	}
-
-	public function validate_get()
-	{
-		$this->load->model('auth/user');
-
-		// $data = $this->user->get_all('username, id, wololo');
-		// var_dump($data);
-		// $data = $this->user->get_all('username, id');
-		// var_dump($data);
-		// $data = $this->user->get(1);
-		// var_dump($data);
-		// $data = $this->user->get(50);
-		// var_dump($data);
-		// $data = $this->user->get_where(['email' => 'ho@ixaya.cpom']);
-		// var_dump($data);
-		// $data = $this->user->get_where(['email' => 'ho5@ixaya.com']);
-		// var_dump($data);
-		// $data = $this->user->get_all('username, id, wololo');
-		// var_dump($data);
-		// $data = $this->user->get_all('username, id, wololo');
-		// var_dump($data);
-		// $data =  $this->user->query("SELECT id FROM user"); //CI_DB_mysqli_result
-		// var_dump($data);
-		// $data =  $this->user->query("UPDATE user_key SET device_uuid = 'AAAaa' where id = 1");//true
-		// $data =  $this->user->query_alter("UPDATE user_key SET device_uuids = 'AAA' where id = 4");//false 
-		// var_dump($data);
-
-		$this->load->driver('cache');
-		// $data = [
-		// 	'name' => "\xB1\x31"
-		// ];
-		$data = ['hello'=>'yes'];
-		$this->cache->save('test', $data, 58);
-		$data = $this->cache->delete('test');
-		var_dump($data);
-
-		$data = (object)['hello' => 'yes'];
-		var_dump($data);
-		$this->cache->save('test_php', $data, 66, false, 'none');
-		$data = $this->cache->get('test_php');
-		var_dump($data);
-	}
 }
